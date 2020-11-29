9266a8d1411b507784498f6f641b5dbb22926811
NguSa1 <sa.nguyen@gmx.de>
2020-11-29 11:54:40 +0100

    clt schubvector etc

--- main.py
+++
@@ -1,7 +0,0 @@
-import calc as c
-
-q_0 = c.stiffness_matrix(0)
-q_45 = c.stiffness_matrix(45)
-A = c.abd(q_0, q_45)
-print(A)
-
--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import numpy as np
 
-d = np.ones(3)
-a = np.reshape(d,(3,1))
-b = np.identity(3)
-print(a)
-print(b)
+A = np.ones((3,3))
+B = np.zeros((3,3))
 
-c = np.concatenate((b,a),axis=1)
+D = np.ones((3,3))*2
 
-print(c)
+
+
+# c = np.concatenate((b,a),axis=1)
+
+
+ABD = np.vstack((np.hstack((A,B)),np.hstack((B,D))))
+e = np.ones(5)
+print(e)
+f = e[0:3]
+print(f)
